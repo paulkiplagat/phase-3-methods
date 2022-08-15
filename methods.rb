3b5b288114ebb16aac874e3222bf0d268379eae1
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-15 15:09:22 +0300

    another comm

--- methods.rb
+++ methods.rb
@@ -2,7 +2,11 @@
 def greet_programmer
     puts "Hello, programmer!"
   end
-  
+
   def greet(name)
+    puts "Hello, #{name}!"
+  end
+
+  def greet_with_default(name = "programmer")
     puts "Hello, #{name}!"
   end
