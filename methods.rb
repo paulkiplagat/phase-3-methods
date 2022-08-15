1658b5fd6e148aae873c978fa785cf9e61a06c03
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-15 15:10:26 +0300

    last commit

--- methods.rb
+++ methods.rb
@@ -10,3 +10,12 @@ def greet_programmer
   def greet_with_default(name = "programmer")
     puts "Hello, #{name}!"
   end
+  def add(num1, num2)
+    num1 + num2
+  end
+  
+  def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
